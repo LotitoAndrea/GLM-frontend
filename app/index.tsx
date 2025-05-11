70486df2b1cd4c4979e78a9666553507dbcd7730
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2025-05-11 14:21:28 +0200

    aggiunto gluestack al progetto assieme a TailWind e NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white text-white">
+        <Text>Welcome to Expo Router</Text>
+        <Text>To get started, edit app/index.tsx</Text>
     </View>
   );
 }
