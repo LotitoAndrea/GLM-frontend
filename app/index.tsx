6184f50e6c2bb3bc00a5a2566362acb89cfdc945
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2025-05-11 14:54:50 +0200

    creata branch registrazione, con l'aggiunta di componente di login e reindirizzamento alla pagina di accesso.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import {Redirect} from "expo-router";
 
 export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white text-white">
-        <Text>Welcome to Expo Router</Text>
-        <Text>To get started, edit app/index.tsx</Text>
-    </View>
-  );
+    return (
+        <View>
+            <Redirect href="/login"/>
+        </View>
+    );
 }
